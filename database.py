be438eb8d6955f985c98658bb312b6cd4f3db055
Syed Ahmad Shah <117304625+AhmadShah-1@users.noreply.github.com>
2026-01-03 19:00:19 -0500

    added connection limit and refresh

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine(os.environ['DB_CONNECTION_STRING'])
+engine = create_engine(os.environ['DB_CONNECTION_STRING'], pool_size=2, max_overflow=0, pool_recycle=1000, pool_pre_ping=True)
 
 def load_jobs_from_db():
     with engine.connect() as conn:
@@ -13,3 +13,13 @@ def load_jobs_from_db():
         for row in result.all():
             jobs.append(row._asdict())
         return jobs
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0])
