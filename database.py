915d03e699202c7634316666461c6b20ce8054b5
Syed Ahmad Shah <117304625+AhmadShah-1@users.noreply.github.com>
2026-01-03 20:12:24 -0500

    dynamic pages

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine(os.environ['DB_CONNECTION_STRING'], pool_size=2, max_overflow=0, pool_recycle=1000, pool_pre_ping=True)
+engine = create_engine(os.environ['DB_CONNECTION_STRING'], pool_size=1, max_overflow=0, pool_recycle=1000, pool_pre_ping=True)
 
 def load_jobs_from_db():
     with engine.connect() as conn:
@@ -22,4 +22,4 @@ def load_job_from_db(id):
         if len(rows) == 0:
             return None
         else:
-            return dict(rows[0])
+            return rows[0]._asdict()
